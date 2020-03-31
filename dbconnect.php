53e54ac8f9d1a97c15ee3970e9b711e7e50315a0
AliSim87 <47104828+AliSim87@users.noreply.github.com>
2020-03-31 09:57:51 +0100

    combining html and php

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,13 @@
 <?php
 
-session_start();
-$DB_HOST = 'sql309.epizy.com';
-$DB_USER = 'epiz_25235579';
-$DB_PASS = 'xaY6JAuPdkv2M';
-$DB_NAME = 'epiz_25235579_gymapp';
+$DB_HOST = 'localhost';
+$DB_USER = 'root';
+$DB_PASS = '';
+$DB_NAME = 'gymapp';
 
 $db = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
-if ( mysqli_connect_errno() ) {
+if (mysqli_connect_errno()) {
     die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 
 }
--- www/logout.php
+++ www/logout.php
@@ -1,4 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("location: index.php");
+// Redirect to the login page:
+header('Location: index.html');
+?>
